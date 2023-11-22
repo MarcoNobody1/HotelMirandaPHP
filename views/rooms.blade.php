b7efe08c9d52cfa78ce264009f25596c46f7ff4d
MarcoNobody1 <marcocamaradiaz@gmail.com>
2023-11-22 16:46:11 +0100

    se crea blade para pagina de Rooms

---
+++ views/rooms.blade.php
@@ -0,0 +1,146 @@
+@extends('layout')
+@section('title')
+Hotel Miranda | Rooms
+@endsection
+@section('content')
+<section class="roomsintro">
+        <div class="roomsintro__inner">
+            <h6 class="roomsintro__pretitle">THE ULTIMATE LUXURY</h6>
+            <h1 class="roomsintro__title">Ultimate Room</h1>
+            <div class="roomsintro__pages">
+                <a class="roomsintro__linkprev" href="/">Home</a>
+                <span class="roomsintro__linkbar">|</span>
+                <span class="roomsintro__linktext">Rooms</span>
+            </div>
+        </div>
+    </section>
+    <section class="roomshow">
+        <div class="rommshow__inner">
+            <div class="roomshow__roomwrapper">
+                <img class="roomshow__photo" src="../assets/Rooms_swiper/room 1.jpg" alt="Hotel Room">
+                <div class="roomshow__icons">
+                    <img src="../assets/Rooms__desc Icons/icon 1.svg" alt="" class="">
+                    <img src="../assets/Rooms__desc Icons/icon 2.svg" alt="" class="">
+                    <img src="../assets/Rooms__desc Icons/icon 3.svg" alt="" class="">
+                    <img src="../assets/Rooms__desc Icons/icon 4.svg" alt="" class="">
+                    <img src="../assets/Rooms__desc Icons/icon 5.svg" alt="" class="">
+                    <img src="../assets/Rooms__desc Icons/icon 6.svg" alt="" class="">
+                    <img src="../assets/Rooms__desc Icons/icon 7.svg" alt="" class="">
+                </div>
+                <div class="roomshow__desc">
+                    <h3 class="roomshow__title">Minimal Duplex Room</h3>
+                    <p class="roomshow__content">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+                        eiusmod
+                        tempor incididunt ut labore et dolore.</p>
+                    <span class="roomshow__price">$345/Night</span>
+                    <span class="roomshow__booknow">Book Now</span>
+                </div>
+            </div>
+            <div class="roomshow__roomwrapper">
+                <img class="roomshow__photo" src="../assets/Rooms_swiper/room 1.jpg" alt="Hotel Room">
+                <div class="roomshow__icons">
+                    <img src="../assets/Rooms__desc Icons/icon 1.svg" alt="" class="">
+                    <img src="../assets/Rooms__desc Icons/icon 2.svg" alt="" class="">
+                    <img src="../assets/Rooms__desc Icons/icon 3.svg" alt="" class="">
+                    <img src="../assets/Rooms__desc Icons/icon 4.svg" alt="" class="">
+                    <img src="../assets/Rooms__desc Icons/icon 5.svg" alt="" class="">
+                    <img src="../assets/Rooms__desc Icons/icon 6.svg" alt="" class="">
+                    <img src="../assets/Rooms__desc Icons/icon 7.svg" alt="" class="">
+                </div>
+                <div class="roomshow__desc">
+                    <h3 class="roomshow__title">Minimal Duplex Room</h3>
+                    <p class="roomshow__content">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+                        eiusmod
+                        tempor incididunt ut labore et dolore.</p>
+                    <span class="roomshow__price">$345/Night</span>
+                    <span class="roomshow__booknow">Book Now</span>
+                </div>
+            </div>
+            <div class="roomshow__roomwrapper">
+                <img class="roomshow__photo" src="../assets/Rooms_swiper/room 1.jpg" alt="Hotel Room">
+                <div class="roomshow__icons">
+                    <img src="../assets/Rooms__desc Icons/icon 1.svg" alt="" class="">
+                    <img src="../assets/Rooms__desc Icons/icon 2.svg" alt="" class="">
+                    <img src="../assets/Rooms__desc Icons/icon 3.svg" alt="" class="">
+                    <img src="../assets/Rooms__desc Icons/icon 4.svg" alt="" class="">
+                    <img src="../assets/Rooms__desc Icons/icon 5.svg" alt="" class="">
+                    <img src="../assets/Rooms__desc Icons/icon 6.svg" alt="" class="">
+                    <img src="../assets/Rooms__desc Icons/icon 7.svg" alt="" class="">
+                </div>
+                <div class="roomshow__desc">
+                    <h3 class="roomshow__title">Minimal Duplex Room</h3>
+                    <p class="roomshow__content">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+                        eiusmod
+                        tempor incididunt ut labore et dolore.</p>
+                    <span class="roomshow__price">$345/Night</span>
+                    <span class="roomshow__booknow">Book Now</span>
+                </div>
+            </div>
+            <div class="roomshow__roomwrapper">
+                <img class="roomshow__photo" src="../assets/Rooms_swiper/room 1.jpg" alt="Hotel Room">
+                <div class="roomshow__icons">
+                    <img src="../assets/Rooms__desc Icons/icon 1.svg" alt="" class="">
+                    <img src="../assets/Rooms__desc Icons/icon 2.svg" alt="" class="">
+                    <img src="../assets/Rooms__desc Icons/icon 3.svg" alt="" class="">
+                    <img src="../assets/Rooms__desc Icons/icon 4.svg" alt="" class="">
+                    <img src="../assets/Rooms__desc Icons/icon 5.svg" alt="" class="">
+                    <img src="../assets/Rooms__desc Icons/icon 6.svg" alt="" class="">
+                    <img src="../assets/Rooms__desc Icons/icon 7.svg" alt="" class="">
+                </div>
+                <div class="roomshow__desc">
+                    <h3 class="roomshow__title">Minimal Duplex Room</h3>
+                    <p class="roomshow__content">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+                        eiusmod
+                        tempor incididunt ut labore et dolore.</p>
+                    <span class="roomshow__price">$345/Night</span>
+                    <span class="roomshow__booknow">Book Now</span>
+                </div>
+            </div>
+            <div class="roomshow__roomwrapper">
+                <img class="roomshow__photo" src="../assets/Rooms_swiper/room 1.jpg" alt="Hotel Room">
+                <div class="roomshow__icons">
+                    <img src="../assets/Rooms__desc Icons/icon 1.svg" alt="" class="">
+                    <img src="../assets/Rooms__desc Icons/icon 2.svg" alt="" class="">
+                    <img src="../assets/Rooms__desc Icons/icon 3.svg" alt="" class="">
+                    <img src="../assets/Rooms__desc Icons/icon 4.svg" alt="" class="">
+                    <img src="../assets/Rooms__desc Icons/icon 5.svg" alt="" class="">
+                    <img src="../assets/Rooms__desc Icons/icon 6.svg" alt="" class="">
+                    <img src="../assets/Rooms__desc Icons/icon 7.svg" alt="" class="">
+                </div>
+                <div class="roomshow__desc">
+                    <h3 class="roomshow__title">Minimal Duplex Room</h3>
+                    <p class="roomshow__content">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+                        eiusmod
+                        tempor incididunt ut labore et dolore.</p>
+                    <span class="roomshow__price">$345/Night</span>
+                    <span class="roomshow__booknow">Book Now</span>
+                </div>
+            </div>
+            <div class="roomshow__roomwrapper">
+                <img class="roomshow__photo" src="../assets/Rooms_swiper/room 1.jpg" alt="Hotel Room">
+                <div class="roomshow__icons">
+                    <img src="../assets/Rooms__desc Icons/icon 1.svg" alt="" class="">
+                    <img src="../assets/Rooms__desc Icons/icon 2.svg" alt="" class="">
+                    <img src="../assets/Rooms__desc Icons/icon 3.svg" alt="" class="">
+                    <img src="../assets/Rooms__desc Icons/icon 4.svg" alt="" class="">
+                    <img src="../assets/Rooms__desc Icons/icon 5.svg" alt="" class="">
+                    <img src="../assets/Rooms__desc Icons/icon 6.svg" alt="" class="">
+                    <img src="../assets/Rooms__desc Icons/icon 7.svg" alt="" class="">
+                </div>
+                <div class="roomshow__desc">
+                    <h3 class="roomshow__title">Minimal Duplex Room</h3>
+                    <p class="roomshow__content">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+                        eiusmod
+                        tempor incididunt ut labore et dolore.</p>
+                    <span class="roomshow__price">$345/Night</span>
+                    <span class="roomshow__booknow">Book Now</span>
+                </div>
+            </div>
+            <img class="roomshow__pagination-cover" src="../assets/Rooms_swiper/Pagination.png" alt="Future Pagination Here!">
+            <span class="roomshow__explanation">Future Pagination Here!</span>
+        </div>
+    </section>
+@endsection
+@section('scripts')
+<script src="/scripts/Rooms.js"></script>
+@endsection
