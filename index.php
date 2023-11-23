3aaed45d623de6805cc9a2461e988e57d5e6b065
MarcoNobody1 <marcocamaradiaz@gmail.com>
2023-11-23 18:20:25 +0100

    se añaden conexion a sql y queries para rooms al index

--- index.php
+++ index.php
@@ -1,9 +1,25 @@
 <?php
 
+require_once("config.php");
 require_once("setup.php");
-$prueba = 'crack';
-$prueba2 = 'que pasa';
-$prueba3 = false;
-echo $blade->run("home");
 
-?>
+$query = 'SELECT r.*,
+GROUP_CONCAT(DISTINCT p.photo_url) as photo,
+GROUP_CONCAT(a.amenity) as amenity
+FROM room r
+LEFT JOIN photos p ON r.id = p.room_id
+LEFT JOIN room_amenities ra ON r.id = ra.room_id
+LEFT JOIN amenity a ON ra.amenity_id = a.id
+GROUP BY r.id;';
+
+$result = $connection->query($query);
+
+$rooms = $result->fetch_all(MYSQLI_ASSOC);
+
+foreach ($rooms as &$room) {
+
+    $room['discountedPrice'] = $room['price'] - ($room['price'] * ($room['discount'] / 100));
+}
+
+
+echo $blade->run("home", ['rooms' => $rooms]);
