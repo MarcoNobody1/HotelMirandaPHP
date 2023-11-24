2699a7354e2c2945ac03400e775cf4f7fc6cd913
MarcoNobody1 <marcocamaradiaz@gmail.com>
2023-11-24 09:21:22 +0100

    se crean queries y se pasan rooms especificas a offers

--- offers.php
+++ offers.php
@@ -1,7 +1,38 @@
 <?php
-
+require_once("config.php");
 require_once("setup.php");
 
-echo $blade->run("offers");
+$query = 'SELECT r.*,
+GROUP_CONCAT(DISTINCT p.photo_url) as photo,
+GROUP_CONCAT(a.amenity) as amenity
+FROM room r
+LEFT JOIN photos p ON r.id = p.room_id
+LEFT JOIN room_amenities ra ON r.id = ra.room_id
+LEFT JOIN amenity a ON ra.amenity_id = a.id
+GROUP BY r.id;';
 
-?>
+$result = $connection->query($query);
+$allRooms = $result->fetch_all(MYSQLI_ASSOC);
+
+foreach ($allRooms as &$room) {
+    $room['discountedPrice'] = $room['price'] - ($room['price'] * ($room['discount'] / 100));
+}
+
+$randomRoomIndices = array_rand($allRooms, 5);
+$randomRooms = array_intersect_key($allRooms, array_flip($randomRoomIndices));
+
+
+
+$discountedRooms = array_filter($randomRooms, function ($room) {
+    return $room['discount'] !== 0;
+});
+
+
+foreach ($discountedRooms as &$room) {
+    $room['discountedPrice'] = $room['price'] - ($room['price'] * ($room['discount'] / 100));
+}
+
+$chunks = array_chunk($discountedRooms, 3);
+
+
+echo $blade->run("offers", ['rooms' => $randomRooms, 'discountedRooms' => $chunks]);
