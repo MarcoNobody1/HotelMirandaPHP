fde34238e2a927470cc168e0491ac085c8995356
MarcoNobody1 <marcocamaradiaz@gmail.com>
2023-11-30 23:22:04 +0100

    se añade el resto de la logica

--- scripts/RoomDetails.js
+++ scripts/RoomDetails.js
@@ -3,7 +3,6 @@ const menuCross = document.getElementById("menuCross");
 const nav = document.getElementById("nav");
 const header = document.querySelector(".header");
 
-
 menuBurguer.addEventListener("click", () => {
   menuBurguer.classList.add("header__menuiconburguer--closed");
   menuCross.classList.remove("header__menuiconcross--closed");
@@ -34,4 +33,52 @@ window.addEventListener("mousemove", (event) => {
   } else if (event.clientY > 210 && window.innerWidth > 999) {
     header.classList.remove("header--float");
   }
+});
+
+function initRoomDetailsSwiper() {
+  var swiper;
+  if (window.matchMedia("(max-width: 1000px)").matches) {
+    swiper = new Swiper("#roomsrelated__swiper", {
+      navigation: {
+        nextEl: "#roomsrelated-nextslide",
+        prevEl: "#roomsrelated-prevslide",
+      },
+    });
+  } else {
+    swiper = new Swiper("#roomsrelated__swiper", {
+      slidesPerView: 2,
+      spaceBetween: 10,
+      keyboard: true,
+      navigation: {
+        nextEl: "#roomsrelated-nextslide",
+        prevEl: "#roomsrelated-prevslide",
+      },
+    });
+  }
+}
+
+initRoomDetailsSwiper();
+
+window.addEventListener("resize", () => {
+  initRoomDetailsSwiper();
+});
+
+const goToRoomDetails = () => {
+
+  window.location.href = 'roomdetails.php';
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  const form = document.getElementById("roomsdetails__form");
+
+  form.addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    Swal.fire({
+      title: "Unsent Verification",
+      text: "In the future, this will be used to check available dates in this specific room!",
+      icon: "info",
+      confirmButtonText: "Ok",
+    });
+  });
 });
