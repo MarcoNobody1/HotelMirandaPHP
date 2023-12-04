289bb1a3bee7e72b7099232877c54fcc22f148bb
MarcoNobody1 <marcocamaradiaz@gmail.com>
2023-12-04 08:59:04 +0100

    se empieza logica de queries

--- rooms.php
+++ rooms.php
@@ -1,15 +1,50 @@
 <?php
+session_start();
 require_once("config.php");
 require_once("setup.php");
 
-$query = 'SELECT r.*,
-GROUP_CONCAT(DISTINCT p.photo_url) as photo,
-GROUP_CONCAT(a.amenity) as amenity
-FROM room r
-LEFT JOIN photos p ON r.id = p.room_id
-LEFT JOIN room_amenities ra ON r.id = ra.room_id
-LEFT JOIN amenity a ON ra.amenity_id = a.id
-GROUP BY r.id;';
+if (isset($_GET["arrival"]) && isset($_GET["departure"])) {
+    $checkin = htmlspecialchars($_GET["arrival"]);
+    $_SESSION['arrival'] = $checkin;
+    $checkout = htmlspecialchars($_GET["departure"]);
+    $_SESSION['arrival'] = $checkout;
+
+    $query = 'SELECT r.*,
+    GROUP_CONCAT(DISTINCT p.photo_url) as photo,
+    GROUP_CONCAT(a.amenity) as amenity
+    FROM room r
+    LEFT JOIN photos p ON r.id = p.room_id
+    LEFT JOIN room_amenities ra ON r.id = ra.room_id
+    LEFT JOIN amenity a ON ra.amenity_id = a.id
+    WHERE r.availability = "Available" AND discount = 0
+    AND NOT EXISTS (
+        SELECT 1
+        FROM booking b
+        WHERE r.id = b.room_id
+        AND (
+            ("$checkin" BETWEEN b.check_in AND b.check_out)
+            OR ("$checkout" BETWEEN b.check_in AND b.check_out)
+            OR (b.check_in BETWEEN "$checkin" AND "$checkout")
+            OR (b.check_out BETWEEN "$checkin" AND "$checkout")
+            )
+    )
+GROUP BY r.id;
+';
+} else {
+
+    $query = 'SELECT r.*,
+    GROUP_CONCAT(DISTINCT p.photo_url) as photo,
+    GROUP_CONCAT(a.amenity) as amenity
+    FROM room r
+    LEFT JOIN photos p ON r.id = p.room_id
+    LEFT JOIN room_amenities ra ON r.id = ra.room_id
+    LEFT JOIN amenity a ON ra.amenity_id = a.id
+    WHERE r.availability = "Available" AND discount = 0
+    GROUP BY r.id;
+';
+}
+
+
 
 $result = $connection->query($query);
 
@@ -18,8 +53,7 @@ $rooms = $result->fetch_all(MYSQLI_ASSOC);
 foreach ($rooms as &$room) {
     $room['discountedPrice'] = $room['price'] - ($room['price'] * ($room['discount'] / 100));
 }
-
+session_destroy();
 $chunks = array_chunk($rooms, 5);
 
 echo $blade->run("rooms", ['rooms' => $chunks]);
-?>
