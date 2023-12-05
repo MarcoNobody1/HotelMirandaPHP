c269a4cbeaba751ccd01a2f8c3a9b4d1a3351891
MarcoNobody1 <marcocamaradiaz@gmail.com>
2023-12-05 08:53:38 +0100

    se empieza logica hacia BBDD

--- roomdetails.php
+++ roomdetails.php
@@ -1,7 +1,52 @@
 <?php
-
+session_start();
+require_once("config.php");
 require_once("setup.php");
 
-echo $blade->run("roomdetails");
 
-?>
+if ($_SERVER["REQUEST_METHOD"] == "GET") {
+    if (isset($_GET['id'])) {
+
+        $checkin = $_SESSION['arrival'];
+        $checkout = $_SESSION['departure'];
+        $id = htmlspecialchars($_GET['id']);
+
+        $query0 = "SELECT r.*,
+        GROUP_CONCAT(DISTINCT p.photo_url) as photo,
+        GROUP_CONCAT(a.amenity) as amenity
+        FROM room r
+        LEFT JOIN photos p ON r.id = p.room_id
+        LEFT JOIN room_amenities ra ON r.id = ra.room_id
+        LEFT JOIN amenity a ON ra.amenity_id = a.id
+        WHERE r.id = $id
+        GROUP BY r.id;";
+    }
+
+    $result0 = $connection->query($query0);
+    $roomDetail = $result0->fetch_assoc();
+    $roomDetail['finalPrice'] = $roomDetail['price'] - ($roomDetail['price'] * ($roomDetail['discount'] / 100));
+
+} else if ($_SERVER["REQUEST_METHOD"] == "GET") {
+
+
+}
+
+
+$query = 'SELECT r.*,
+GROUP_CONCAT(DISTINCT p.photo_url) as photo,
+GROUP_CONCAT(a.amenity) as amenity
+FROM room r
+LEFT JOIN photos p ON r.id = p.room_id
+LEFT JOIN room_amenities ra ON r.id = ra.room_id
+LEFT JOIN amenity a ON ra.amenity_id = a.id
+WHERE r.availability = "Available" AND discount = 0
+GROUP BY r.id;';
+
+$result = $connection->query($query);
+$allRooms = $result->fetch_all(MYSQLI_ASSOC);
+$randomRoomIndices = array_rand($allRooms, 5);
+$recommendedRooms = array_intersect_key($allRooms, array_flip($randomRoomIndices));
+
+
+
+echo $blade->run("roomdetails", ['recommendedRooms' => $recommendedRooms, 'roomdetails' => $roomDetail, 'checkin' => $checkin, 'checkout' => $checkout]);
