baf9d388f19a748b1ec254a47145b5cecec9102e
MarcoNobody1 <marcocamaradiaz@gmail.com>
2023-12-05 15:39:51 +0100

    se termina logica de la query

--- offers.php
+++ offers.php
@@ -1,16 +1,51 @@
 <?php
+session_start();
 require_once("config.php");
 require_once("setup.php");
 
-$query = 'SELECT r.*,
-GROUP_CONCAT(DISTINCT p.photo_url) as photo,
-GROUP_CONCAT(a.amenity) as amenity
-FROM room r
-LEFT JOIN photos p ON r.id = p.room_id
-LEFT JOIN room_amenities ra ON r.id = ra.room_id
-LEFT JOIN amenity a ON ra.amenity_id = a.id
-WHERE discount != 0
-GROUP BY r.id;';
+if ($_SERVER["REQUEST_METHOD"] == "GET") {
+
+    if (isset($_GET["arrival"]) && isset($_GET["departure"])) {
+
+        $checkin =  $_SESSION['arrival'];
+        $checkout =  $_SESSION['departure'];
+    }
+
+
+    $query = "SELECT r.*,
+    GROUP_CONCAT(DISTINCT p.photo_url) as photo,
+    GROUP_CONCAT(a.amenity) as amenity
+    FROM room r
+    LEFT JOIN photos p ON r.id = p.room_id
+    LEFT JOIN room_amenities ra ON r.id = ra.room_id
+    LEFT JOIN amenity a ON ra.amenity_id = a.id
+    WHERE r.availability = 'Available' AND discount != 0
+    AND NOT EXISTS (
+        SELECT 1
+        FROM booking b
+        WHERE r.id = b.room_id
+        AND (
+            ('$checkin' BETWEEN b.check_in AND b.check_out)
+            OR ('$checkout' BETWEEN b.check_in AND b.check_out)
+            OR (b.check_in BETWEEN '$checkin' AND '$checkout')
+            OR (b.check_out BETWEEN '$checkin' AND '$checkout')
+            )
+    )
+    GROUP BY r.id;
+";
+} else {
+
+    $query = 'SELECT r.*,
+    GROUP_CONCAT(DISTINCT p.photo_url) as photo,
+    GROUP_CONCAT(a.amenity) as amenity
+    FROM room r
+    LEFT JOIN photos p ON r.id = p.room_id
+    LEFT JOIN room_amenities ra ON r.id = ra.room_id
+    LEFT JOIN amenity a ON ra.amenity_id = a.id
+    WHERE discount != 0
+    GROUP BY r.id;';
+}
+
 
 $result = $connection->query($query);
 $discountedRooms = $result->fetch_all(MYSQLI_ASSOC);
