f8924c9d0882959049b898cc296d363adf7182df
MarcoNobody1 <marcocamaradiaz@gmail.com>
2023-12-05 15:40:05 +0100

    se corrigen errores de sintaxis

--- rooms.php
+++ rooms.php
@@ -9,27 +9,28 @@ if (isset($_GET["arrival"]) && isset($_GET["departure"])) {
     $checkout = htmlspecialchars($_GET["departure"]);
     $_SESSION['departure'] = $checkout;
 
-    $query = 'SELECT r.*,
+    $query = "SELECT r.*,
     GROUP_CONCAT(DISTINCT p.photo_url) as photo,
     GROUP_CONCAT(a.amenity) as amenity
     FROM room r
     LEFT JOIN photos p ON r.id = p.room_id
     LEFT JOIN room_amenities ra ON r.id = ra.room_id
     LEFT JOIN amenity a ON ra.amenity_id = a.id
-    WHERE r.availability = "Available"
+    WHERE r.availability = 'Available'
     AND NOT EXISTS (
         SELECT 1
         FROM booking b
         WHERE r.id = b.room_id
         AND (
-            ("$checkin" BETWEEN b.check_in AND b.check_out)
-            OR ("$checkout" BETWEEN b.check_in AND b.check_out)
-            OR (b.check_in BETWEEN "$checkin" AND "$checkout")
-            OR (b.check_out BETWEEN "$checkin" AND "$checkout")
+            ('$checkin' BETWEEN b.check_in AND b.check_out)
+            OR ('$checkout' BETWEEN b.check_in AND b.check_out)
+            OR (b.check_in BETWEEN '$checkin' AND '$checkout')
+            OR (b.check_out BETWEEN '$checkin' AND '$checkout')
             )
     )
-GROUP BY r.id;
-';
+    GROUP BY r.id;
+";
+
 } else {
 
     $query = 'SELECT r.*,
