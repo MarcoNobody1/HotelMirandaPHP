ddb04dc94e40174f1b49c5491417fe7fd9b50a37
MarcoNobody1 <marcocamaradiaz@gmail.com>
2023-12-07 21:13:33 +0100

    se modifica logica en rooms para controlar los casos en los que no hay dechas seleccionadas

--- rooms.php
+++ rooms.php
@@ -30,7 +30,32 @@ if (isset($_GET["arrival"]) && isset($_GET["departure"])) {
     )
     GROUP BY r.id;
 ";
+} else if (isset($_SESSION['arrival']) && isset($_SESSION['departure'])) {
+    $checkin = $_SESSION['arrival'];
+    $checkout = $_SESSION['departure'];
 
+
+    $query = "SELECT r.*,
+    GROUP_CONCAT(DISTINCT p.photo_url) as photo,
+    GROUP_CONCAT(a.amenity) as amenity
+    FROM room r
+    LEFT JOIN photos p ON r.id = p.room_id
+    LEFT JOIN room_amenities ra ON r.id = ra.room_id
+    LEFT JOIN amenity a ON ra.amenity_id = a.id
+    WHERE r.availability = 'Available'
+    AND NOT EXISTS (
+        SELECT 1
+        FROM booking b
+        WHERE r.id = b.room_id
+        AND (
+            ('$checkin' BETWEEN b.check_in AND b.check_out)
+            OR ('$checkout' BETWEEN b.check_in AND b.check_out)
+            OR (b.check_in BETWEEN '$checkin' AND '$checkout')
+            OR (b.check_out BETWEEN '$checkin' AND '$checkout')
+            )
+    )
+    GROUP BY r.id;
+";
 } else {
 
     $query = 'SELECT r.*,
@@ -57,4 +82,4 @@ foreach ($rooms as &$room) {
 
 $chunks = array_chunk($rooms, 5);
 
-echo $blade->run("rooms", ['rooms' => $chunks]);
+echo $blade->run("rooms", ['rooms' => $chunks, 'checkin' => $checkin, 'checkout' => $checkout]);
